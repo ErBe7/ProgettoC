662c0e3a4cf900f28373e4db14a83fa0b2e1bb20
besomarco825@gmail.com <besomarco825@gmail.com>
2024-04-24 13:53:13 +0200

    Mappa Livello 1

--- Livello1.cpp
+++ Livello1.cpp
@@ -2,6 +2,31 @@
 #include <string>
 
 int main() {
-	char Emilio[] = "Ciao";
-	printf(Emilio);
+    char Livello1[10][10] = {0};
+    int i = 0;
+    int j = 0;
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (i == 0 || i == 10 - 1) {
+                Livello1[i][j] = '_';
+            }
+            else {
+                if (j == 0 || j == 10 - 1) {
+                        Livello1[i][j] = '|';
+                }
+                else {
+                    Livello1[i][j] = ' ';
+                }
+            }
+        }
+    }
+
+    for (i = 0;i < 10;i++) {
+        for (j = 0;j < 10;j++) {
+            printf("%c ", Livello1[i][j]);
+        }
+        printf("\n");
+    }
+
 }
