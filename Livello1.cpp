396e876262120369f6f1e67da2e28b7f105f3145
besomarco825@gmail.com <besomarco825@gmail.com>
2024-04-24 15:40:17 +0200

    Fatto la mappa

--- Livello1.cpp
+++ Livello1.cpp
@@ -1,32 +1,35 @@
-#include <stdlib.h>
-#include <string>
+#include <stdio.h>
 
-int main() {
-    char Livello1[10][10] = {0};
-    int i = 0;
-    int j = 0;
+#define DIMENSIONE 45
 
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            if (i == 0 || i == 10 - 1) {
-                Livello1[i][j] = '_';
-            }
-            else {
-                if (j == 0 || j == 10 - 1) {
-                        Livello1[i][j] = '|';
-                }
-                else {
-                    Livello1[i][j] = ' ';
-                }
-            }
-        }
-    }
-
-    for (i = 0;i < 10;i++) {
-        for (j = 0;j < 10;j++) {
-            printf("%c ", Livello1[i][j]);
+void printTable(int Livello1[DIMENSIONE][DIMENSIONE]) {
+    for (int i = 0; i < DIMENSIONE; i++) {
+        printf("\t\t\t\t\t\t");
+        for (int j = 0; j < DIMENSIONE; j++) {
+            printf("%d ", Livello1[i][j]);
         }
         printf("\n");
     }
+}
 
+int main() {
+    int Livello1[DIMENSIONE][DIMENSIONE];
+
+    // Popolamento della matrice con 0
+    for (int i = 0; i < DIMENSIONE; i++) {
+        for (int j = 0; j < DIMENSIONE; j++) {
+            // Imposta 1 sui bordi, sulle righe e sulle colonne 2, 4, 6 e 8
+            if (i == 0 || i == DIMENSIONE - 1 || j == 0 || j == DIMENSIONE - 1 || i % 2 != 1 || j % 2 != 1) {
+                Livello1[i][j] = 1;
+            }
+            else {
+                Livello1[i][j] = 0;
+            }
+        }
+    }
+
+    // Stampa della tabella
+    printTable(Livello1);
+
+    return 0;
 }
